91e4fa7c3e0d5523bc5a01f110961c1b7dec0511
Chris Davies <dev@christophilus.com>
2024-08-11 18:20:57 +0000

    Add breakpoint and animations to the demo page
    Move the focus rule in index.css into @layer base.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -18,6 +18,41 @@ function Page() {
           </code>{' '}
           to edit this and play around.
         </p>
+        <footer class="flex gap-4 mt-4 flex-col sm:flex-row">
+          <button
+            class="bg-pink-600 active:bg-pink-500 text-white rounded-full p-2 px-4 cursor-pointer transition-all outline-pink-500"
+            onClick={(e: any) => {
+              (e.target as HTMLButtonElement).classList.toggle('animate-pulse');
+            }}
+          >
+            Click to pulse
+          </button>
+
+          <button
+            class="bg-sky-600 active:bg-sky-500 text-white rounded-full p-2 px-4 cursor-pointer relative transition-all"
+            onClick={(e) => {
+              const target = e.currentTarget as HTMLButtonElement;
+              const spinner = target.querySelector('.js-spin');
+              const content = target.querySelector('.js-content')!;
+              if (spinner) {
+                spinner.remove();
+                content.classList.remove('invisible');
+                return;
+              }
+              content.classList.add('invisible');
+              content.insertAdjacentHTML(
+                'afterend',
+                [
+                  `<span class="js-spin flex items-center justify-center absolute inset-0">`,
+                  `<span class="inline-block border-2 border-current border-t-transparent animate-spin size-6 rounded-full"></span>`,
+                  `</span>`,
+                ].join(''),
+              );
+            }}
+          >
+            <span class="js-content">Click to spin</span>
+          </button>
+        </footer>
       </section>
     </HtmlPage>
   );
